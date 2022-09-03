ac6224e089f8253715f14124552a5954fb1e0f60
Uladzislau Radkevich <uladzislau.radkevich@gmail.com>
2022-09-03 16:04:55 +0200

    Initial on-ramp flow implementation

--- snap/src/index.ts
+++ snap/src/index.ts
@@ -5,23 +5,56 @@ type InitiateOnRampRequest = {
   redirectUrl: string,
 }
 
-export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
+const supportedAssets = [
+  'ETH_ETH',
+  'MATIC_MATIC',
+  'MATIC_USDC',
+  'MATIC_DAI',
+];
+
+const mapSwapAssets = (chainId: string, asset: string): string => {
+  let chainName = '';
+  switch (chainId) {
+    case '0x4': // rinkeby
+      chainName = 'ETH';
+      break;
+    case '0x13881': // matic mumbai
+      chainName = 'MATIC';
+      break;
+    default:
+      throw new Error('Unsupported chain');
+  }
+
+  const result = chainName + '_' + asset;
+  if (supportedAssets.indexOf(result) === -1) {
+    console.log(result);
+    throw new Error('Unsupported asset');
+  }
+
+  return result;
+}
+
+export const onRpcRequest: OnRpcRequestHandler = async ({ origin, request }) => {
   switch (request.method) {
     case 'initiateOnRamp':
       const id = crypto.randomUUID();
 
-      // todo: get address
-      // todo: get chain
+      const addresses = await wallet.request({ method: 'eth_requestAccounts' });
+      const walletAddress = addresses[0];
+      const chainId = await wallet.request({ method: 'eth_chainId' });
 
-      const swapAsset = encodeURIComponent('ETH_ETH');
+      const swapAsset = encodeURIComponent(mapSwapAssets(chainId, request.asset));
+      const swapAmount = encodeURIComponent(request.amount);
+      const userAddress = encodeURIComponent(walletAddress);
+      const hostAppName = encodeURIComponent('Ramap');
+      const webhookStatusUrl = encodeURIComponent('https://c4e5-31-61-163-250.eu.ngrok.io');
 
-      const hostAppName = encodeURIComponent('MetaMask on-ramp');
-      const url = encodeURIComponent('https://widget.hackaton.ramp-network.org');
-
-      const onRampUrl = 'https://buy.ramp.network/?' +
+      const onRampUrl = 'https://widget.hackaton.ramp-network.org?' +
+        `hostAppName=${hostAppName}` +
         `&swapAsset=${swapAsset}` +
-        `&hostAppName=${hostAppName}` +
-        `&url=${url}` +
+        `&swapAmount=${swapAmount}` +
+        `&userAddress=${userAddress}` +
+        `&webhookStatusUrl=${webhookStatusUrl}` +
         '';
 
       return Promise.resolve({
